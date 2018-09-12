c43246f00205bcee890b026a247f840838df1716
Svetlana.Matveeva <matveevas94@yandex.ru>
2018-09-12 18:11:04 +0300

    C1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import datetime
 import sys
 import xml
+import re
 
 
 apikey = '3d4db511-c803-4569-a638-e0babb6cbccc'
@@ -27,13 +28,27 @@ resp = r.json()
 payload1 = {'apikey': apikey, 'format': json}
 # r1 = requests.get(api_url_base1, payload1)
 r1 = requests.get('https://api.rasp.yandex.net/v3.0/stations_list/?apikey=3d4db511-c803-4569-a638-e0babb6cbccc&lang=ru_ru&format=json')
-r1.encoding = 'utf-8'
-
 print(r1)
-resp1 = r1.json()
-b=b'\u0421\u0443\u0440\u0430\u043c\u0438'
-b1 = b.decode('utf-8')
-print(b1)
+
+with open('places.json', 'r') as f:
+    distros_dict = json.load(f)
+print((distros_dict['countries'][2]).keys())
+print(((distros_dict['countries'])[1]['regions'][0]['settlements'][0]['stations'][0]).keys())
+print(type(distros_dict))
+# for i in (distros_dict['countries']):
+#     print((distros_dict['countries'][i])['title'])
+# print(distros_dict)
+# for distro in distros_dict:
+# #     print(distro)
+# print(type(r1.text))
+# res1 = list()
+# res1.append(re.findall('Сурами',r1.text ))
+# print(res1)
+# resp1 = r1.json()
+# print(resp1)
+# b='\u0421\u0443\u0440\u0430\u043c\u0438'
+# b.encode('utf-8')
+# print(b)
 
 # resp1 = r1.json()
 # resp1.text
