ad5297b5000d331ed8df830cd8b0f99373cd94d2
Svetlana.Matveeva <matveevas94@yandex.ru>
2018-09-14 17:53:13 +0300

    C1

--- main.py
+++ main.py
@@ -18,6 +18,30 @@ date = datetime.datetime.now().strftime('%Y-%M-%d')#now.strftime("%Y-%m-%d %H:%M
 #url = api_url_base+'apikey='+apikey+'&'+'format='+format+'&'+'from='+from1+'&'+'to='+to+'&'+'lang='+lang+'&'+'date='+date
 payload = {'apikey': apikey,'from':from1, 'to': to,  'format': format}
 print(payload)
+
+# input data
+from_inp = input("Enter from:")
+to_inp = input("Enter to: ")
+#  download json with codes
+with open('codes.json', 'r') as c:
+     codes = json.load(c)
+print(type(codes))
+print(codes)
+codes_dict={}
+for i in codes:
+    #codes_dict.fromkeys(i[0]:i[1])
+    codes_dict.fromkeys(i[0],i[1])
+    # print(i[0])
+    # print(i[1])
+mydict= {k: v for k, v in  codes[0][0]}
+print(mydict.values())
+print(type(codes_dict))
+print(codes_dict.get('Уруссу'))
+    # for v,k in i:
+    #     print("V"+v+"  "+ "k"+k)
+# codes_dict = dict()
+# codes_dict[codes[0][0]:codes[0][1]]
+# print(codes_dict)
 r = requests.get(api_url_base, payload)
 print(r)
 resp = r.json()
